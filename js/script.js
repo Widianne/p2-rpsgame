052fdaaf3998d5d6e53d292ba31d8ad183aac170
WIDIANNE_DALVALLEN <widiannedalvallen@gmail.com>
2023-11-16 18:01:35 +0100

    feat: google font added

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// what beats what
+
+// need the buttons and know when someone clixks
+
+// need to know wht button was clicked
+
+// need to know what their hand beats
+
+// get a cpu guess
+
+// check who wins
+
+// if the plyer wins then playerScore + 1
+
+// if the cpu wins then cpuScore + 1
+
+// if it's a draw then move on
+
+// game ends when someone has a socre of 10
+
+// allow user to reset and play again
