92380265974bd56c18e82f3828056436d3d521de
WIDIANNE_DALVALLEN <widiannedalvallen@gmail.com>
2023-11-17 23:56:28 +0100

    git push chages

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
+/*the code declares variables to store information for the 
+rock-paper-scissors game. computer/player score, initialized to 0*/ 
 var player;
 var playerScore = 0;
 var computer;
 var computerScore = 0;
 
+/*array intended to represent the possible choices in game*/ 
 var choices = ["rock", "paper", "scissors"];
 
+/* event listeners for each button inside the element with the 
+class "choices." adds a "click" event listener to each button
+when clicked to call a function */
 window.onload = function() {
   let buttons = document.querySelectorAll(".choices button");
   buttons.forEach(button => {
     button.addEventListener("click", selectChoice);
   });
+
+  /*reset button*/
+  let resetButton = document.getElementById("reset");
+  resetButton.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+    document.getElementById("player-score").innerText = playerScore;
+    document.getElementById("computer-score").innerText = computerScore;
+
+    // Reset player and computer choices images
+    document.getElementById("player-choice").src = "assets/imgs/rps-img/hand.jpg";
+    document.getElementById("computer-choice").src = "assets/imgs/rps-img/hand.jpg";
+  });
 };
 
+/*updates the displayed images for both the player's and computer's choices*/
 function selectChoice() {
   player = this.id;
   document.getElementById("player-choice").src = "assets/imgs/rps-img/" + player + ".jpg";
@@ -19,7 +39,7 @@ function selectChoice() {
   computer = choices[Math.floor(Math.random() * 3)];
   document.getElementById("computer-choice").src = "assets/imgs/rps-img/" + computer + ".jpg";
 
-  // Check for the winner
+  /*determines the outcome of a round in a rock-paper-scissors game and updates the scores*/
   if (player === computer) {
     playerScore += 1;
     computerScore += 1;
@@ -35,43 +55,7 @@ function selectChoice() {
     }
   }
 
-
-  resetButton.addEventListener('click', () => {
-    playerScore = 0;
-    computerScore = 0;
-    playerScoreDisplay.innerText = '';
-    computerScoreDisplay.innerText = '';
-    canPlay = true;
-  });
-
-
+  /*sets the text inside these elements to the respective scores*/
   document.getElementById("player-score").innerText = playerScore;
   document.getElementById("computer-score").innerText = computerScore;
 }
-
-
-
-
-
-
-// what beats what
-
-// need the buttons and know when someone clixks
-
-// need to know wht button was clicked
-
-// need to know what their hand beats
-
-// get a cpu guess
-
-// check who wins
-
-// if the plyer wins then playerScore + 1
-
-// if the cpu wins then cpuScore + 1
-
-// if it's a draw then move on
-
-// game ends when someone has a socre of 10
-
-// allow user to reset and play again
